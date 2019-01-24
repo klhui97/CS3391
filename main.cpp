0c237ae6a1e8227b18c7c8713bf3d9b84404be01
KL <huikldavid@gmail.com>
2019-01-24 20:40:36 +0800

    add lib

--- main.cpp
+++ main.cpp
@@ -5,76 +5,38 @@
 #include <string.h>
 #include <stdlib.h>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
-
-int weight(char &c) {
-    if (c == '-' || c == '+') {
-        return 1;
-    }else if (c == '*' || c == '/') {
-        return 2;
-    }else {
-        return 0;
-    }
-}
+int scroe[1000];
 
 void mainFunction() {
     int N;
     cin >> N;
-    cin.ignore();
-    cin.ignore();
-    
-    while (N--) {
-        string line;
-        stack<char> s;
-        char ans[51];
-        int cnt = 0;
-        
-        while (true) {
-            getline(cin, line);
-            
-            if (line.length() == 0 || !cin) {
-                break;
-            }
-            
-            char c = line[0];
-            
-            if (isdigit(c)) {
-                ans[cnt++] = c;
-            }else if (c == ')') {
-                while (s.top() != '(') {
-                    ans[cnt++] = s.top();
-                    s.pop();
-                }
-                s.pop();
-            }else if (c == '(') {
-                s.push(c);
-            }else {
-                // operators
-                if (s.empty()) {
-                    s.push(c);
-                }else {
-                    while (!s.empty() && weight(s.top()) >= weight(c)) {
-                        ans[cnt++] = s.top();
-                        s.pop();
-                    }
-                    s.push(c);
-                }
-            }
-        }
 
-        while (!s.empty()) {
-            ans[cnt++] = s.top();
-            s.pop();
+    while(N--){
+        int students;
+        cin >> students;
+        int i = 0;
+        int total = 0;
+        while(i < students){
+            cin >> scroe[i];
+            total += scroe[i];
+            i++;
         }
-        ans[cnt] = '\0';
-        if (N == 0) {
-            cout << ans << endl;
-        }else {
-            cout << ans << endl << endl;
+        int pass = 0;
+        double avg = total / students;
+
+        i = 0;
+        while(i < students){
+            if (scroe[i] > avg) {
+                pass++;
+            }
+            i++;
         }
-        
+        cout << fixed << setprecision(3) << pass * 1.0 / students * 1.0 * 100 << "%" << endl;
     }
+    
 }
 
 int main() {
